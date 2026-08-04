1b17c701bffb1c1a7cb2ce539cd91fd8f38e3be1
Pantane1 <pantane254@gmail.com>
2026-08-04 12:36:27 +0300

    Fix bookController.js filename casing

--- backend/src/routes/libraryRoutes.js
+++ backend/src/routes/libraryRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bookController = require('../controllers/BookController');
+const bookController = require('../controllers/bookController');
 const borrowingController = require('../controllers/borrowingController');
 const { buildCrudRouter } = require('../utils/routeFactory');
 
