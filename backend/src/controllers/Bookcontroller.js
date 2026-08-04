c4861038fa0dce708629e284bb4cd7d7ef3f0057
𝑃𝑎𝑛𝑡𝑎𝑛𝑒_§ <pantane254@gmail.com>
2026-08-04 12:19:10 +0300

    Delete backend/src/controllers/Bookcontroller.js

--- backend/src/controllers/Bookcontroller.js
+++
@@ -1,6 +0,0 @@
-const { createCrudController } = require('../utils/crudFactory');
-
-module.exports = createCrudController('books', {
-  allowedFields: ['title', 'author', 'isbn', 'category', 'total_copies', 'available_copies'],
-  orderBy: 'title',
-});
